562dbe584c85974335daaf7e8d03eeb95f0090df
hezhengxu <hzx.tony@hotmail.com>
2023-06-20 22:08:56 +0800

    feat: add proxy service.

--- app/common/constants.ts
+++ app/common/constants.ts
@@ -1,9 +1,11 @@
 export const BUG_VERSIONS = 'bug-versions';
 export const LATEST_TAG = 'latest';
 export const GLOBAL_WORKER = 'GLOBAL_WORKER';
+export const PROXY_MODE_CACHED_PACKAGE_DIR_NAME = 'proxy-mode-cached-packages';
 export enum SyncMode {
   none = 'none',
   admin = 'admin',
+  proxy = 'proxy',
   exist = 'exist',
   all = 'all',
 }
